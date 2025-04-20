f20ccefd3d88445741d4c5b39949276a68b99acb
AlbertoPanza <albertopanza@hotmail.it>
2025-04-20 16:25:33 +0200

    Create planets.py

---
+++ planets.py
@@ -0,0 +1,65 @@
+planets = {
+	'Mercury' : {
+		'semi-major axis (meters)' : 57.909e9,
+		'orbital period (days)' : 87.97,
+		'perihelion (au)' : 0.31,
+		'aphelion (au)' : 0.47,
+		'inclination (degrees)' : 7.0
+	},
+	'Venus' : {
+		'semi-major axis (meters)' : 108.21e9,
+		'orbital period (days)' : 224.70,
+		'perihelion (au)' : 0.72,
+		'aphelion (au)' : 0.73,
+		'inclination (degrees)' : 3.4
+	},
+	'Earth' : {
+		'semi-major axis (meters)' : 149.598e9,
+		'orbital period (days)' : 365.26,
+		'perihelion (au)' : 0.98,
+		'aphelion (au)' : 1.02,
+		'inclination (degrees)' : 0.0
+	},
+	'Mars' : {
+		'semi-major axis (meters)' : 227.956e9,
+		'orbital period (days)' : 686.98,
+		'perihelion (au)' : 1.38,
+		'aphelion (au)' : 1.67,
+		'inclination (degrees)' : 1.8
+	},
+	'Jupiter' : {
+		'semi-major axis (meters)' : 778.479e9,
+		'orbital period (days)' : 4331.86,
+		'perihelion (au)' : 4.95,
+		'aphelion (au)' : 5.45,
+		'inclination (degrees)' : 1.3
+	},
+	'Saturn' : {
+		'semi-major axis (meters)' : 1432.041e9,
+		'orbital period (days)' : 10760.265,
+		'perihelion (au)' : 9.01,
+		'aphelion (au)' : 10.07,
+		'inclination (degrees)' : 2.5
+	},
+	'Uranus' : {
+		'semi-major axis (meters)' : 2867.043e9,
+		'orbital period (days)' : 30684.65,
+		'perihelion (au)' : 18.28,
+		'aphelion (au)' : 20.09,
+		'inclination (degrees)' : 0.8
+	},
+	'Neptune' : {
+		'semi-major axis (meters)' : 4514.953e9,
+		'orbital period (days)' : 60189.55,
+		'perihelion (au)' : 29.80,
+		'aphelion (au)' : 30.32,
+		'inclination (degrees)' : 1.8
+	},
+	'Pluto' : {
+		'semi-major axis (meters)' : 5869.656e9,
+		'orbital period (days)' : 90560,
+		'perihelion (au)' : 29.66,
+		'aphelion (au)' : 49.66,
+		'inclination (degrees)' : 17.1
+	}
+}
